c08e75743ef4cbf30af618efe064f5e33ce9ea7e
vatsal3333 <vatsalseth33@gmail.com>
2018-08-13 00:03:41 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+ 
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {console.log('Server is up on ${port}');})
